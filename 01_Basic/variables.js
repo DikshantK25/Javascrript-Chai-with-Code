d7100f7198ead7d1d3f36e68bd74afd5acf827be
DikshantK25 <153298654+DikshantK25@users.noreply.github.com>
2025-02-09 05:25:15 +0000

    coversion

---
+++ 01_Basic/variables.js
@@ -0,0 +1,8 @@
+const accountId =1444
+let accountEmail ="dikshant@gmail.com"
+var accountPasssword="13783"
+accountCity="Jaipur"
+let accountState
+
+console.table([accountId,accountEmail,accountPasssword,accountCity,accountState])
+
